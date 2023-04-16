8ea611cadfd65c84c3cfc606300ee71d0301b0c3
Wang24022003 <quangle.24022003@gmail.com>
2023-04-16 10:07:11 +0700

    update file Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,9 @@ namespace WinFormsApp4
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Bao oi toi doi
+        }
     }
 }
